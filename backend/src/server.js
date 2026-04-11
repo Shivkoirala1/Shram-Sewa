289f7835d7f0f5a8162d8e4a1156a41541f49774
Shivkoirala1 <koirala.khushi456@gmail.com>
2026-04-11 19:32:50 +0545

    Adding the entry point to the server

---
+++ backend/src/server.js
@@ -0,0 +1,42 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js";
+import jobRoutes from "./routes/jobs.js";
+import adminRoutes from "./routes/admin.js";
+import paymentRoutes from "./routes/payment.js";
+import userRoutes from "./routes/users.js";
+import reviewRoutes from "./routes/reviews.js";
+import notificationRoutes from "./routes/notifications.js";
+// import geoRoutes from './routes/location.js'; 
+
+dotenv.config();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Connect DB
+connectDB();
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/jobs", jobRoutes);
+app.use("/api/admin", adminRoutes);
+app.use('/api/payment', paymentRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/notifications", notificationRoutes);
+// app.use('/api/location', geoRoutes); 
+
+// Test API
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
